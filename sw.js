9d371e2edff9a569eed32a403481a4edcf84be73
Hikazyo <hikazyo@163.com>
2024-08-30 04:32:52 +0800

    Site updated: 2024-08-30 04:32:50

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1724962380970";
+const VERSION = "1724963570119";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
